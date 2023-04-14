0b879ea8badca578a6391e502c59a920e195dd2c
Lili Karashchuk <krchtchk@gmail.com>
2023-04-13 17:09:29 -0700

    simplify commit

--- chat.el
+++ chat.el
@@ -128,19 +128,13 @@ If no key is found, error."
 
 
 
-(defun remove-bad-messages (messages)
-  "Filters out items in MESSAGES with no content.
-MESSAGES is a list of items, where each item is a list of (key . value) pairs.
-Returns a new list of items where each item has a non-nil 'content' key.
-Makes it still work when user sends nothing in chat."
-  (seq-filter (lambda (item) (not (null (cdr (assoc "content" item))))) messages))
 
 (defun chat--async-raw (raw-messages callback finalize)
   "Query ChatGPT with MESSAGES.
 CALLBACK is called in the receiving buffer with POINT at the
 beginning of the new data.
 FINALIZE is called after all data has been processed."
-  (let* ((messages (remove-bad-messages raw-messages))
+  (let* ((messages (seq-filter (lambda (item) (cdr (assoc "content" item))) messages))
          (url-request-method "POST")
          (url-request-extra-headers `(("Content-Type" . "application/json")
                                       ("Authorization" . ,(concat "Bearer " (chat-get-api-key)))))
